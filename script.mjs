9ec4b10c3e48d1690dfe7dcc6e373170cc548f9d
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-10-05 11:13:41 +0100

    Add topics display container

--- script.mjs
+++ script.mjs
@@ -6,7 +6,4 @@
 
 import { getUserIds } from "./common.mjs";
 
-window.onload = function () {
-  const users = getUserIds();
-  document.querySelector("body").innerText = `There are ${users.length} users`;
-};
+
